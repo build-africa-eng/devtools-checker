8ac1ac8eb6c5b4342d9b449fb2561a2d86776a12
build-africa-eng <afrcanfuture@gmail.com>
2025-06-07 14:08:56 +0300

    Create shared/types.js

---
+++ shared/types.js
@@ -0,0 +1,10 @@
+// @typedef {Object} ConsoleLog
+// @property {string} type - Log type (error, warning, info)
+// @property {string} text - Log message
+// @property {Object} location - Source location
+
+// @typedef {Object} NetworkRequest
+// @property {string} url - Request URL
+// @property {string} method - HTTP method
+// @property {string} resourceType - Resource type (xhr, script, etc.)
+// @property {number|null} status - HTTP status code
