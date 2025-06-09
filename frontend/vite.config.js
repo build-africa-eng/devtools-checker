4628e78100794c97b201881c60d438705a88ba4c
build-africa-eng <afrcanfuture@gmail.com>
2025-06-09 12:31:31 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: 'frontend', // <--- Tell Vite to use frontend/index.html as the entry!
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
