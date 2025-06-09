9d1bf6a3a509f094df4060fc703ab183b8915220
build-africa-eng <afrcanfuture@gmail.com>
2025-06-09 12:54:44 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // ✅ ensure this is set
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
