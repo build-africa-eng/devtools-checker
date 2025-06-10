07484e90474e9f8f05e17de4af288a7701327ed1
build-africa-eng <afrcanfuture@gmail.com>
2025-06-10 14:37:22 +0300

    Update launchBrowser.js

--- backend/utils/puppeteer/launchBrowser.js
+++ backend/utils/puppeteer/launchBrowser.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer-extra');
 const { LAUNCH_ARGS, MOBILE_DEVICES } = require('./constants');
-const { logger } = require('../logger');
+const { logger } = require('../utils/logger');
 
 async function launchBrowserWithRetries(retries = 3, device = 'iPhone 12', customDevice = null, debug = false) {
   for (let i = 0; i < retries; i++) {
